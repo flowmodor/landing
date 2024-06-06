b1592eead7d2c3f25ebaf8636c9d6d48e3e87586
Max Shen <m4xshen@gmail.com>
2024-06-06 15:34:00 +0800

    feat: add lastmod to sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://flowmodor.com',
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap({
+      lastmod: new Date('2024-06-06'),
+    }),
+  ],
   trailingSlash: 'never',
 });
